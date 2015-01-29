e17cc3c8fc71b327e9d553f973fba7af66184021
dutu <adrian.clinciu@outlook.com>
2015-01-29 19:22:00 +0200

    add all methods

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-var lang = "en";
-var errMsg = require("./lib/errors_" +lang);
+
+"use strict";
 
 function Cryptox (exchangeSlug, options) {
     var self = this;
-    var Exchange = require('./lib/' + exchangeSlug);
+    var Exchange = require("./lib/" + exchangeSlug);
     self.properties = Exchange.prototype.properties;
 
-    var exchange;
-    // TODO: Check exchangeSlug?
-    exchange = new Exchange(options);
+    var exchange = new Exchange(options);
 
     self.getTicker = function (options, callback){
         exchange.getTicker(options, function (err, ticker){
@@ -16,9 +14,57 @@ function Cryptox (exchangeSlug, options) {
         });
     }
 
+    self.getOrderBook = function (options, callback){
+        exchange.getOrderBook(options, function (err, orderBook){
+            callback(err, orderBook);
+        });
+    }
+
+    self.getTrades = function (options, callback){
+        exchange.getTrades(options, function (err, trades){
+            callback(err, trades);
+        });
+    }
+
     self.getFee = function (options, callback){
         exchange.getFee(options, function (err, fee){
             callback(err, fee);
+        });
+    }
+
+    self.getTransactions = function (options, callback){
+        exchange.getTransactions(options, function (err, transactions){
+            callback(err, transactions);
+        });
+    }
+
+    self.getBalance = function (options, callback){
+        exchange.getBalance(options, function (err, balance){
+            callback(err, balance);
+        });
+    }
+
+    self.getOpenOrders = function (options, callback){
+        exchange.getOpenOrders(options, function (err, openOrders){
+            callback(err, openOrders);
+        });
+    }
+
+    self.postSellOrder = function (options, callback){
+        exchange.postSellOrder(options, function (err, orderResult){
+            callback(err, orderResult);
+        });
+    }
+
+    self.postBuyOrder = function (options, callback){
+        exchange.postBuyOrder(options, function (err, orderResult){
+            callback(err, orderResult);
+        });
+    }
+
+    self.cancelOrder = function (options, callback){
+        exchange.cancelOrder(options, function (err, orderResult){
+            callback(err, orderResult);
         });
     }
 }
